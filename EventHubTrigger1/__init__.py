1721c170c98dfcd1c8bffcdf80d771dcce45983b
mani3482 <mbajaj@zuelligpharma.com>
2023-08-18 14:03:45 +0800

    updated

---
+++ EventHubTrigger1/__init__.py
@@ -0,0 +1,35 @@
+import logging
+
+import azure.functions as func
+import config
+import azure.cosmos.documents as documents
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+from azure.cosmos.partition_key import PartitionKey
+import json
+import datetime
+import time
+import requests
+import xmltodict
+
+logging.basicConfig(level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s:\t%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+log = logging.getLogger(__name__)
+log.info("Logging Info")
+log.debug("Logging Debug")
+
+def main(events: func.EventHubEvent, doc: func.Out[func.Document]) -> str:
+    for event in events:
+        event_body=event.get_body().decode('utf-8')
+        log.info('Python EventHub trigger processed an xml event: %s',
+                    event_body)
+        
+        doc.set(func.Document.from_json(event_body))
+        log.info('Sent record to cosmos DB')
+        #url='http://sgesbisapp.dfs.com:5555/Inventory/SellingLocation?Division=58&Store=451&Register=632&SKU=18299120&Indicator=R&FromSellingLocation=451'
+        #response = requests.post(url)
+        #log.info(response.status_code)
+        return event_body
+
